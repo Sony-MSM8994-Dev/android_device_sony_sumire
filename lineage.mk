c60166c7ec3466b44e8efa996c923902cbc27dd0
Olivier Karasangabo <olivier@lineageos.org>
2017-11-02 22:17:58 +0100

    sumire: Use Lineage vendor
    Change-Id: I9dec5304fc62785a40233998bc0906c176d6465e

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,6 @@
 $(call inherit-product, device/sony/sumire/full_sumire.mk)
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_sumire
