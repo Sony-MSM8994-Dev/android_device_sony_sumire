e80ac2fca6a92abb3381234de6635c1f60c5841d
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:26:16 +0100

    sumire: Consider lineage_sumire product via the AOSP way
    Change-Id: Iabdaacb8f29b2e268b2d076077e4c042a6198174

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-$(call inherit-product, device/sony/sumire/full_sumire.mk)
-
-# Inherit some common CM stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_sumire
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_sumire.mk
