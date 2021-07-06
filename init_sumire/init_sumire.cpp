45ddc702ff69b04c217c637e49796e4c52031f93
LuK1337 <priv.luk@gmail.com>
2021-07-06 14:51:11 +0200

    sumire: read model from lta-label

--- init_sumire/init_sumire.cpp
+++ init_sumire/init_sumire.cpp
@@ -21,8 +21,10 @@
 #include <android-base/logging.h>
 
 #include <fcntl.h>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <unistd.h>
 #include <sys/stat.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
@@ -34,6 +36,8 @@
 #include "util.h"
 
 using android::init::ImportKernelCmdline;
+
+constexpr auto LTALABEL_PATH = "/dev/block/platform/soc.0/f9824900.sdhci/by-name/LTALabel";
 
 void property_override(char const prop[], char const value[], bool add = true)
 {
@@ -64,25 +68,31 @@ static void import_kernel_nv(const std::string& key,
 {
     if (key.empty()) return;
 
+    if (std::ifstream file = std::ifstream(LTALABEL_PATH, std::ios::binary)) {
+        std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        size_t offset = str.find("Model: ");
+
+        if (offset != std::string::npos) {
+            std::string model = str.substr(offset + strlen("Model: "), 5);
+            property_override("ro.semc.product.model", model.c_str());
+            property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", model.c_str());
+            property_override_triple("ro.product.name", "ro.product.system.name", "ro.product.vendor.name", model.c_str());
+        }
+    }
+
     if (key == "oemandroidboot.phoneid") {
         // Dual Sim variant contains two IMEIs separated by comma.
         if ((count(value.begin(), value.end(),',')) > 0) {
             property_override("persist.multisim.config", "dsds");
             property_override("persist.radio.multisim.config", "dsds");
             property_override("ro.telephony.default_network", "9,1");
-            property_override("ro.semc.product.model", "E6683");
             property_override("ro.semc.product.name", "Xperia Z5 Dual");
-            property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "E6683");
-            property_override_dual("ro.product.name", "ro.vendor.product.name", "sumire_dsds");
             property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "sumire_dsds");
             property_override("ro.build.description", "E6683-user 7.1.1 32.4.A.1.54 3761073091 release-keys");
             property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "Sony/E6683/E6683:7.1.1/32.4.A.1.54/3761073091:user/release-keys");
         } else {
             property_override("ro.telephony.default_network", "9");
-            property_override("ro.semc.product.model", "E6653");
             property_override("ro.semc.product.name", "Xperia Z5");
-            property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "E6653");
-            property_override_dual("ro.product.name", "ro.vendor.product.name", "sumire");
             property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "sumire");
             property_override("ro.build.description", "E6653-user 7.1.1 32.4.A.1.54 3761073091 release-keys");
             property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "Sony/E6653/E6653:7.1.1/32.4.A.1.54/3761073091:user/release-keys");
